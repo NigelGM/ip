b1c94f1e5287679a3b9d7c0d7226a83ac1c3bfbb
NigelGM <nigelgmorais@gmail.com>
2026-01-27 06:06:10 +0800

    Revert "Level-3 task class and mark/unmark"
    This reverts commit 83280aa868923c1e46e09db92fc78bece05a5b49.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,11 +2,11 @@ public class TaskList {
 
     private static final int MAX_TASKS = 100;
 
-    private final Task[] tasks = new Task[MAX_TASKS];
+    private final String[] tasks = new String[MAX_TASKS];
     private int taskCount = 0;
 
-    public void add(String description) {
-        tasks[taskCount] = new Task(description);
+    public void add(String task) {
+        tasks[taskCount] = task;
         taskCount++;
     }
 
@@ -14,22 +14,8 @@ public class TaskList {
         return taskCount;
     }
 
-    public Task get(int index) {
+    public String get(int index) {
         return tasks[index];
     }
-
-    // user types "mark 2" -> index = 1
-    public Task mark(int oneBasedIndex) {
-        Task t = tasks[oneBasedIndex - 1];
-        t.markDone();
-        return t;
-    }
-
-    public Task unmark(int oneBasedIndex) {
-        Task t = tasks[oneBasedIndex - 1];
-        t.unmark();
-        return t;
-    }
 }
-
 
