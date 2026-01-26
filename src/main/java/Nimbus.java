ee1939edec6719ba87e326d59f90510f1b708a52
NigelGM <nigelgmorais@gmail.com>
2026-01-26 12:21:46 +0800

    Level-1 echo loop

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Nimbus {
 
     private static final String LINE =
             "____________________________________________________________";
+    private static final String INDENT = "    ";
 
     public static void main(String[] args) {
         new Nimbus().run();
@@ -9,20 +12,44 @@ public class Nimbus {
 
     public void run() {
         showGreeting();
-        showGoodbye();
+        System.out.println(); // <-- blank line before first user command
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine().trim();
+
+            if (input.equals("bye")) {
+                showGoodbye();
+                System.out.println(); // <-- optional; blank line after goodbye
+                break;
+            }
+
+            echo(input);
+            System.out.println(); // <-- blank line before next user command
+        }
     }
 
     private void showGreeting() {
-        System.out.println(LINE);
-        System.out.println("  Hello! I'm Nimbus");
-        System.out.println("  What can I do for you?");
-        System.out.println(LINE);
+        System.out.println(INDENT + LINE);
+        System.out.println(INDENT + "Hello! I'm Nimbus");
+        System.out.println(INDENT + "What can I do for you?");
+        System.out.println(INDENT + LINE);
+    }
+
+    private void echo(String msg) {
+        System.out.println(INDENT + LINE);
+        System.out.println(INDENT + msg);
+        System.out.println(INDENT + LINE);
     }
 
     private void showGoodbye() {
-        System.out.println("  Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        System.out.println(INDENT + LINE);
+        System.out.println(INDENT + "Bye. Hope to see you again soon!");
+        System.out.println(INDENT + LINE);
     }
 }
+
+
 
 
