f71f822313b03a593432136856dead40a8e4531d
NigelGM <nigelgmorais@gmail.com>
2026-01-26 12:27:25 +0800

    Revert "Level-1 echo loop"
    This reverts commit ee1939edec6719ba87e326d59f90510f1b708a52.

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 public class Nimbus {
 
     private static final String LINE =
             "____________________________________________________________";
-    private static final String INDENT = "    ";
 
     public static void main(String[] args) {
         new Nimbus().run();
@@ -12,44 +9,20 @@ public class Nimbus {
 
     public void run() {
         showGreeting();
-        System.out.println(); // <-- blank line before first user command
-
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            String input = sc.nextLine().trim();
-
-            if (input.equals("bye")) {
-                showGoodbye();
-                System.out.println(); // <-- optional; blank line after goodbye
-                break;
-            }
-
-            echo(input);
-            System.out.println(); // <-- blank line before next user command
-        }
+        showGoodbye();
     }
 
     private void showGreeting() {
-        System.out.println(INDENT + LINE);
-        System.out.println(INDENT + "Hello! I'm Nimbus");
-        System.out.println(INDENT + "What can I do for you?");
-        System.out.println(INDENT + LINE);
-    }
-
-    private void echo(String msg) {
-        System.out.println(INDENT + LINE);
-        System.out.println(INDENT + msg);
-        System.out.println(INDENT + LINE);
+        System.out.println(LINE);
+        System.out.println("  Hello! I'm Nimbus");
+        System.out.println("  What can I do for you?");
+        System.out.println(LINE);
     }
 
     private void showGoodbye() {
-        System.out.println(INDENT + LINE);
-        System.out.println(INDENT + "Bye. Hope to see you again soon!");
-        System.out.println(INDENT + LINE);
+        System.out.println("  Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
-
-
 
 
